916944dda72f28ef30de2f37c89856a7d8d55341
jacoacoacob <jacoacoacob@protonmail.com>
2023-06-08 09:21:02 -0500

    readme

---
+++ README.md
@@ -0,0 +1,21 @@
+# Wikidata ID Finder
+
+This project contains code that will read a CSV file containing author names and then scrape Wikipedia for their Wikidata IDs.
+
+
+## Running locally
+
+This project relies on [Poetry](https://python-poetry.org/docs/) to execute scripts and manage dependencies in a python virtual environment.
+
+1. Clone or download this repo.
+2. Inside the project root, run 
+   ```bash
+   CSV_DATA=path/to/author/name/csv poetry run fetch-wikipedia-pages
+   ```
+   to download the wikipedia pages for all authors. It's a good idea to not run this more than once so that wikipedia doesn't get upset with all your rapid-fire requests.
+3. Run
+   ```bash
+   CSV_DATA=path/to/author/name/csv poetry run get-wikidata-ids
+   ```
+   This will create a new CSV file called `sandbox-results.csv` in the project root containing the author names,
+   the URL for the page downloaded by `fetch-wikipedia-pages`, and the Wikidata ID found on that page (if present).
--- wikidata_id_scraper/__init__.py
+++ wikidata_id_scraper/__init__.py
@@ -1,10 +1,19 @@
 import os
 import csv
 import requests
-import json
+import re
+from typing import List
 
 
-def get_wikipedia_urls():
+HTML_DIR = os.path.abspath("html")
+
+
+def read_author_html(author_name):
+    with open(HTML_DIR + "/" + author_name + ".html", "r") as f:
+        return f.read()
+
+
+def get_author_names_and_urls() -> List[dict]:
     data_file_path = os.getenv("CSV_DATA")
 
     if not data_file_path:
@@ -25,7 +34,38 @@ def get_wikipedia_urls():
     return urls
 
 
-def main():
-    urls = get_wikipedia_urls()
-    print(json.dumps(urls[:10], indent=1))
+def fetch_wikipedia_pages():
+    if not os.path.exists(HTML_DIR):
+        os.mkdir(HTML_DIR)
 
+    authors = get_author_names_and_urls()
+
+    for index, author in enumerate(authors):
+        print(f"fetching {index + 1} of {len(authors)}")
+        author_name = author.get("author_name")
+        url = author.get("url")
+        response = requests.get(url)
+        with open(HTML_DIR + "/" + author_name + ".html", "w") as f:
+            f.write(response.text)
+
+
+def get_wikidata_ids():
+    authors = get_author_names_and_urls()
+
+    authors_and_wikidata_ids = []
+
+    for author in authors:
+        html = read_author_html(author.get("author_name"))
+        match = re.search(r"Special:EntityPage/(Q\d+)\"", html)
+        if match:
+            author.update({ "wikidata_id": match.group(1) })
+        else:
+            author.update({ "wikidata_id": None })
+        authors_and_wikidata_ids.append(author)
+    
+    with open("sandbox-results.csv", "w") as f:
+        writer = csv.DictWriter(f, fieldnames=authors_and_wikidata_ids[0].keys())
+        writer.writeheader()
+        writer.writerows(authors_and_wikidata_ids)
+        
+    
